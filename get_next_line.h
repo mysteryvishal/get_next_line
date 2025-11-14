30a45692553ad07288cac443d58075118d93f4bc
Vishal Mistry <vmistry@student.42london.com>
2025-11-14 13:58:06 +0000

    added some comments git header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 13:17:09 by vmistry           #+#    #+#             */
-/*   Updated: 2025/11/14 13:55:08 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/11/14 13:57:37 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+// main function
 char	*get_next_line(int fd);
+
+// helper functions
 
 #endif
