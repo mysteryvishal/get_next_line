edddf634056a9a7860906f1573861237e8ad198f
Vishal Mistry <vmistry@student.42london.com>
2025-11-29 15:55:27 +0000

    fix: updated header includes to include fcntl.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,11 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 13:17:09 by vmistry           #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/11/29 15:52:51 by vmistry          ###   ########.fr       */
+=======
+/*   Updated: 2025/11/29 15:16:52 by vmistry          ###   ########.fr       */
+>>>>>>> cbdf5f1 (fix: updated header includes to include fcntl.h)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +21,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
