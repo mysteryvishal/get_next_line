f5c5c0577d965be2be8c4e5a9b50c90a702278ce
Vishal Mistry <vishalmistry51@gmail.com>
2025-11-29 09:35:06 +0000

    add: inital start to get_next_line function

--- get_next_line.c
+++ get_next_line.c
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vmistry <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 13:07:55 by vmistry           #+#    #+#             */
-/*   Updated: 2025/11/14 13:07:58 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/11/16 12:56:49 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	int		bytes_read;
+	// char	*buffer;
+	char	*cup_buffer;
+
+	cup_buffer = ft_calloc(4, sizeof(char));
+	if (!cup_buffer)
+		return (NULL);
+	bytes_read = read(fd, cup_buffer, 3);
+	if (bytes_read <= 0)
+		return (NULL);
+	return (cup_buffer);
+}
