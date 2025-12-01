9edf53e669e8e7e65fda368c8073bb644bf7023c
Vishal Mistry <vmistry@student.42london.com>
2025-12-01 19:06:27 +0000

    refactor: remove ft_memcpy and simplify ft_strjoin implementation

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,31 +6,11 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 13:20:32 by vmistry           #+#    #+#             */
-/*   Updated: 2025/12/01 11:34:33 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/12/01 19:05:57 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-// static functions
-static void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	unsigned char	*to;
-	unsigned char	*from;
-	size_t			i;
-
-	if (!dst && !src)
-		return (dst);
-	to = (unsigned char *) dst;
-	from = (unsigned char *) src;
-	i = 0;
-	while (i < n)
-	{
-		to[i] = from[i];
-		i++;
-	}
-	return (to);
-}
 
 // utils functions
 void	*ft_calloc(size_t nitems, size_t size)
@@ -86,6 +66,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*joined_str;
 	size_t	len_s1;
 	size_t	len_s2;
+	size_t	i;
 
 	if (!s1 || !s2)
 		return (NULL);
@@ -94,8 +75,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	joined_str = (char *)malloc(len_s1 + len_s2 + 1);
 	if (!joined_str)
 		return (NULL);
-	ft_memcpy(joined_str, s1, len_s1);
-	ft_memcpy(joined_str + len_s1, s2, len_s2);
+	i = 0;
+	while (i < (len_s1 + len_s2))
+	{
+		if (s1[i])
+			joined_str[i] = s1[i];
+		if (s2[i - len_s1] && i >= len_s1)
+			joined_str[i] = s2[i - len_s1];
+		i++;
+	}
 	joined_str[len_s1 + len_s2] = '\0';
 	return (joined_str);
 }
